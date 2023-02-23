933c4829238663fe755c7bd59087734187c527b3
bjornoleh <bjorn.haugsgjerd@gmail.com>
2023-02-23 11:28:13 +0100

    Install zip/unzip by default

--- ns-setup-recreate.sh
+++ ns-setup-recreate.sh
@@ -18,7 +18,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin zip unzip
 
 # sudo docker compose up -d
 
